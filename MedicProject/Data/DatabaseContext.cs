6b0ca969d94e85c31d809b8f7d8c605bb322a7cf
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-10-24 18:35:09 +0300

    Created an Database Connection with Oracle Cloud Database

---
+++ MedicProject/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using MedicProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicProject.Data
+{
+    public class DatabaseContext: DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
---
+++ MedicProject/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedicProject.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string email { get; set; }
+
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+    }
+}
