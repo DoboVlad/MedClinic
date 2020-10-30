a76fe32fd768aeaa9c20eac082dfb3f7571965f8
CoroDaniel <danielcoroian15@gmail.com>
2020-10-30 13:04:46 +0200

    Components

--- MedicProject/web-frontend/src/app/app.module.ts
+++ MedicProject/web-frontend/src/app/app.module.ts
@@ -6,13 +6,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { DoctorsComponent } from './Components/doctors/doctors.component';
+import { AppointmentsComponent } from './Components/appointments/appointments.component';
+import { ProfileComponent } from './Components/profile/profile.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DoctorsComponent,
+    AppointmentsComponent,
+    ProfileComponent,
+    ForgotPasswordComponent
   ],
   imports: [
     BrowserModule,
