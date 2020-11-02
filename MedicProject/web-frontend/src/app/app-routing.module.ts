48a07ce3111fd57c107a4c91c9f682393cba6436
CoroDaniel <danielcoroian15@gmail.com>
2020-11-02 16:05:21 +0200

    Create Register page

--- MedicProject/web-frontend/src/app/app-routing.module.ts
+++ MedicProject/web-frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: "**", component: HomeComponent}
 ];
 
