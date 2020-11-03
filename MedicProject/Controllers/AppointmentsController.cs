369a1779e9a364e2a7961c687dabee096a68018f
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-11-03 14:49:53 +0200

    Created a method to get all the appointments of a medic

--- MedicProject/Controllers/AppointmentsController.cs
+++ MedicProject/Controllers/AppointmentsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -55,7 +56,7 @@ namespace MedicProject.Controllers
              return await _context.APPOINTMENTS.AnyAsync(x => x.hour == Hour);
         }
 
-        // return all the appointemnts made by a user
+        // return all the appointements made by a user
         // TODO: Find a way to make this method asynchronous
         [HttpGet("{userId}")]
         public IQueryable<Appointments> getAppointments(int userId){
@@ -64,6 +65,54 @@ namespace MedicProject.Controllers
             // WHERE a.userId = userId
             var appointments =  _context.APPOINTMENTS.FromSqlRaw("SELECT * FROM APPOINTMENTS").Where(p => p.UserId == userId);
             return appointments;
+        }
+
+        //return all the appointements of a medic
+        [HttpGet("allDoctorApp/{id}")]
+        public async Task<ActionResult<IList>> getAllUsers(int id){
+
+            //get all users from db
+            var users =  await _context.USERS.ToListAsync();
+
+            //get all appointments from db
+            var appointemnts = await _context.APPOINTMENTS.ToListAsync();
+
+            //join appointments with users lists
+            var medicAppointemnts = appointemnts.Join(
+                users,
+                keyFromAppointemnts => keyFromAppointemnts.UserId,
+                keyFromUsers => keyFromUsers.Id,
+                (appointements, users) => new {
+                    doctorId = users.doctorId,
+                    pactientFirstName = users.firstName,
+                    pacientLastName = users.lastName,
+                    phone = users.phoneNumber,
+                    email = users.email,
+                    DateOfApp = appointements.date,
+                    HourOfApp = appointements.hour
+                }
+            );
+
+            //join users with medicAppointments lists 
+            var result = users.Join(
+                medicAppointemnts,
+                keyFromUser => keyFromUser.Id,
+                keyFromMedic => keyFromMedic.doctorId,
+                (medic, appointements) => new {
+                    MedicId = medic.Id,
+                    pactientFirstName = appointements.pactientFirstName,
+                    pacientLastName = appointements.pacientLastName,
+                    phone = appointements.phone,
+                    email = appointements.email,
+                    DateOfApp = appointements.DateOfApp,
+                    HourOfApp = appointements.HourOfApp
+                }
+            );
+
+            // find only the correct results
+            var finalResult = result.Where(medic => medic.MedicId == id).ToList();
+
+            return finalResult;
         }
     }
 }
