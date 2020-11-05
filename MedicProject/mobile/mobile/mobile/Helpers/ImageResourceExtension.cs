0501c4fbdf58fcbb9c55e0ea311c8d3a70e1cd4d
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2020-11-05 18:48:31 +0200

    Created an image class, created log in page

---
+++ MedicProject/mobile/mobile/mobile/Helpers/ImageResourceExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile.Helpers
+{
+    //to add images, add the image in the images folder and set the action, in properties, to embedded image
+
+    [ContentProperty(nameof(Source))] //to make sure the content si a source type
+
+    public class ImageResourceExtension : IMarkupExtension
+    {   // IMarkupExtension is needed  
+        public string Source
+        {
+            get; set;
+        }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+            var imageResource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageResource;
+        }
+    }
+}
