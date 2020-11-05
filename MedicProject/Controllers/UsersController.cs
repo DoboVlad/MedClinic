423ab223439f764cd2ae834365a671b308a5d751
Kotai Melinda <kotai_melinda2000@yahoo.com>
2020-11-05 20:12:26 +0200

    Added search functionality

--- MedicProject/Controllers/UsersController.cs
+++ MedicProject/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -143,6 +144,26 @@ namespace MedicProject.Controllers
             _context.SaveChanges();
             return Ok();
         }
+
+
+        [HttpGet]
+        [Route("searchUser")]
+        public async Task<ActionResult<IEnumerable<PatientDTO>>> searchUser(string s){
+            var users = await _context.USERS.Where(p => p.firstName.Contains(s) || p.lastName.Contains(s) || Convert.ToString(p.firstName) +" "+ Convert.ToString(p.lastName) == s || Convert.ToString(p.lastName)+" "+ Convert.ToString(p.firstName) == s).ToListAsync();
+            var userstoreturn = _mapper.Map<IEnumerable<PatientDTO>>(users);
+            return Ok(userstoreturn);
+        
+        }
+
+
+
+        [HttpGet]
+        [Route("searchDoctor")]
+        public async Task<ActionResult<IEnumerable<DoctorDTO>>> searchDoctor(string s){
+        var users = await _context.USERS.Where(p => p.firstName.Contains(s) || p.lastName.Contains(s) || Convert.ToString(p.firstName) +" "+ Convert.ToString(p.lastName) == s || Convert.ToString(p.lastName)+" "+ Convert.ToString(p.firstName) == s).ToListAsync();
+        var userstoreturn = _mapper.Map<IEnumerable<DoctorDTO>>(users);
+        return Ok(userstoreturn);
+       }
 
 
         [HttpPost("register")]
