5c848fbd98f76dd105024cfb3583957188971202
CoroDaniel <danielcoroian15@gmail.com>
2020-11-05 14:18:39 +0200

    Added missing routes

--- MedicProject/web-frontend/src/app/app-routing.module.ts
+++ MedicProject/web-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
+import { AppointmentsComponent } from './Components/appointments/appointments.component';
+import { DoctorsComponent} from './Components/doctors/doctors.component';
+import { ProfileComponent} from './Components/profile/profile.component';
+import { ForgotPasswordComponent } from './Components/forgot-password/forgot-password.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'appointments', component: AppointmentsComponent},
+  {path: 'doctors', component: DoctorsComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent },
+  {path: 'forgotpassword', component: ForgotPasswordComponent},
   {path: "**", component: HomeComponent}
 ];
 
