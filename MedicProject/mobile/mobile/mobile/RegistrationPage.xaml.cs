6478c1a0e2bacab6b0b6b2a47da9a114987fec4f
MARIA-COSMINA ANEGRULUI <maria.anegrului@stud.ubbcluj.ro>
2020-11-06 18:09:57 +0200

    Created register page, created register succeeded page, added validation

---
+++ MedicProject/mobile/mobile/mobile/RegistrationPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32.SafeHandles;
+using mobile.Resources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mobile
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        public RegistrationPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void btnRegister_Clicked(object sender, EventArgs e)
+        {
+            Animations.Button_Scale_Clicked((Button)sender);
+
+            // to verify if the date of birth was chosen
+            int result = DateTime.Compare(dpBirthDate.Date, DateTime.Today);
+
+            //verify if fiels are filled in
+            if (result==0 ||  entFirstName == null || entLastName == null || entEmail == null || entPhone == null || entCnp == null ||  entPlaceOfBirth == null || entAdress == null || pckDoctor== null)
+            {
+                await DisplayAlert("You didn't complete all the fields.", "Would you like to try again?", "Yes", "No");
+            }
+            else if(entPassword != entConfPwd )
+            {
+                await DisplayAlert("Your password don't match.", "Would you like to try again?", "Yes", "No");
+            }
+
+
+            //email validation
+            var email = entEmail.Text;
+            Regex emailPattern = new Regex(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", RegexOptions.CultureInvariant | RegexOptions.Singleline);
+            if (!emailPattern.IsMatch(email)) { 
+               await DisplayAlert ("Your email is invalid.", "Would you like to try again?", "Yes", "No");
+            }
+            else
+            {
+                await this.Navigation.PushAsync(new RegistrationSucceedPage());
+            }
+            
+        }
+    }
+}
--- MedicProject/mobile/mobile/mobile/Resources/AppResources.Designer.cs
+++ MedicProject/mobile/mobile/mobile/Resources/AppResources.Designer.cs
@@ -70,6 +70,15 @@ namespace mobile.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Current adress.
+        /// </summary>
+        internal static string Adress {
+            get {
+                return ResourceManager.GetString("Adress", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to All Rights Reserved..
         /// </summary>
         internal static string AllRightsRes {
@@ -79,11 +88,65 @@ namespace mobile.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Choose a doctor.
+        /// </summary>
+        internal static string ChooseDoctor {
+            get {
+                return ResourceManager.GetString("ChooseDoctor", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Personal identification number.
+        /// </summary>
+        internal static string Cnp {
+            get {
+                return ResourceManager.GetString("Cnp", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Confirm password.
+        /// </summary>
+        internal static string ConfPwd {
+            get {
+                return ResourceManager.GetString("ConfPwd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to First name.
+        /// </summary>
+        internal static string FirstName {
+            get {
+                return ResourceManager.GetString("FirstName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Forgotten password?.
         /// </summary>
         internal static string ForgottenPass {
             get {
                 return ResourceManager.GetString("ForgottenPass", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Click here to go to main page..
+        /// </summary>
+        internal static string GoMainPage {
+            get {
+                return ResourceManager.GetString("GoMainPage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Last name.
+        /// </summary>
+        internal static string LastName {
+            get {
+                return ResourceManager.GetString("LastName", resourceCulture);
             }
         }
         
@@ -106,11 +169,47 @@ namespace mobile.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Phone number.
+        /// </summary>
+        internal static string Phone {
+            get {
+                return ResourceManager.GetString("Phone", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Place of birth.
+        /// </summary>
+        internal static string PlaceOfBirth {
+            get {
+                return ResourceManager.GetString("PlaceOfBirth", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Register.
         /// </summary>
         internal static string Register {
             get {
                 return ResourceManager.GetString("Register", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Your registartion is successful. Please wait for the doctor to accept your request..
+        /// </summary>
+        internal static string RegistrationSucceed {
+            get {
+                return ResourceManager.GetString("RegistrationSucceed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sign up!.
+        /// </summary>
+        internal static string SingUp {
+            get {
+                return ResourceManager.GetString("SingUp", resourceCulture);
             }
         }
         
