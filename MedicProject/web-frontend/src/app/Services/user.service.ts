7fe6ff05674b0aee78abab0302b0487124b9b0ca
DoboVlad <66554619+DoboVlad@users.noreply.github.com>
2020-11-09 19:21:46 +0200

    Added the token and user id in local storage. Can't test it yet, need the backend services.

--- MedicProject/web-frontend/src/app/Services/user.service.ts
+++ MedicProject/web-frontend/src/app/Services/user.service.ts
@@ -9,7 +9,7 @@ export class UserService {
   baseUrl: String = "https://localhost:5001";
   constructor(private http: HttpClient) { }
   user: User;
-
+  token: string;
   //use post method to login the user
   //call the base url using /login endpoint
   logInUser(user: User){
@@ -22,5 +22,31 @@ export class UserService {
     this.http.post(this.baseUrl + "/register", user).subscribe(user =>{
       this.user = user;
     });
+  }
+
+  autoAuthUser(){
+    const authInfo = this.getAuthData();
+  }
+
+  saveAuthData(token: string, id: number){
+    localStorage.setItem('token', token);
+    localStorage.setItem('id', id.toString());
+  }
+
+  clearAuthData(){
+    localStorage.removeItem("token");
+    localStorage.removeItem("id");
+  }
+
+  getAuthData(){
+    const token = localStorage.getItem("token");
+    const id = localStorage.getItem("id");
+    if(token && id){
+      return;
+    }
+    return{
+      token: token,
+      id: id
+    }
   }
 }
--- MedicProject/web-frontend/src/app/app.component.ts
+++ MedicProject/web-frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './Services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  constructor(private userService: UserService){}
+
+  ngOnInit(){
+    this.userService.autoAuthUser();
+  }
 }
